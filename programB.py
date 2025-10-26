2488ece5d1a26cc9c925c6f7f070c801e3a4a932
nico1095 <agiso@students.kennesaw.edu>
2025-10-26 06:16:04 -0400

    Update programB.py

--- programB.py
+++ programB.py
@@ -1,1 +1,7 @@
-
+# Program Name: programB.py 
+# Course: IT3883/Section W02
+# Student Name: Anthony Giso
+# Assignment Number: 4
+# Due Date: 10/26/ 2025
+# two small python programs to implement basic network communications
+# Used Python module and chapters.
