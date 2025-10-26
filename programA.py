e61ca78c471ad268e67ca9433b6056bc88cedd42
nico1095 <agiso@students.kennesaw.edu>
2025-10-26 06:18:56 -0400

    Update programA.py

--- programA.py
+++ programA.py
@@ -5,3 +5,25 @@
 # Due Date: 10/26/ 2025
 # two small python programs to implement basic network communications
 # Used Python module and chapters.
+import socket
+
+
+HOST = '127.0.0.1'
+PORT = 40001
+
+# makes tcp
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+# string input
+message = input("Enter a message to send to Program B: ")
+
+
+client_socket.sendall(message.encode())
+
+
+response = client_socket.recv(1024)
+print(f"Received back from Program B: {response.decode()}")
+
+
+client_socket.close()
